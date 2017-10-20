81de1d6938bd8df6c545668d7d75a76e3b64d9d0
Yuchen Wang <yulon@me.com>
2017-10-20 11:08:57 +0800

    Revised

--- include/nob/util/shared_obj.hpp
+++ include/nob/util/shared_obj.hpp
@@ -4,10 +4,10 @@
 #include <typeindex>
 #include <type_traits>
 
-template <typename S>
+template <typename T>
 class dynamic_shared_obj;
 
-template <typename S>
+template <typename T>
 class shared_obj {
 	protected:
 		constexpr shared_obj() {}
@@ -15,7 +15,7 @@ class shared_obj {
 	public:
 		template<typename... A>
 		void alloc(A&&... a) {
-			_m = std::make_shared<S>(std::forward<A>(a)...);
+			_m = std::make_shared<T>(std::forward<A>(a)...);
 		}
 
 		void free() {
@@ -26,15 +26,15 @@ class shared_obj {
 			return _m.use_count();
 		}
 
-		bool operator==(const shared_obj<S> &src) const {
+		bool operator==(const shared_obj<T> &src) const {
 			return _m.get() == src._m.get();
 		}
 
-		bool operator==(const S *mp) const {
+		bool operator==(const T *mp) const {
 			return _m.get() == mp;
 		}
 
-		bool operator!=(const S *mp) const {
+		bool operator!=(const T *mp) const {
 			return _m.get() != mp;
 		}
 
@@ -50,94 +50,106 @@ class shared_obj {
 			return _m.get();
 		}
 
-		S *operator->() {
+		T *operator->() {
 			return _m.get();
 		}
 
-		const S *operator->() const {
+		const T *operator->() const {
 			return _m.get();
 		}
 
-		S &operator*() {
+		T &operator*() {
 			return *_m;
 		}
 
-		const S &operator*() const {
+		const T &operator*() const {
 			return *_m;
 		}
 
-		typedef S structure;
+		typedef T access_type;
 
 		template<typename D>
 		D cast_with_type() const {
-			static_assert(std::is_base_of<dynamic_shared_obj<typename D::structure>, D>::value, "dest is not a 'dynamic_shared_obj'");
+			static_assert(std::is_base_of<dynamic_shared_obj<typename D::access_type>, D>::value, "dest is not a 'dynamic_shared_obj'");
 
 			D dest;
-			static_cast<dynamic_shared_obj<typename D::structure> &>(dest) = typeid(structure);
-			static_cast<shared_obj<typename D::structure> &>(dest) = std::static_pointer_cast<typename D::structure>(_m);
+			if (_m.get()) {
+				static_cast<dynamic_shared_obj<typename D::access_type> &>(dest) = typeid(access_type);
+				static_cast<shared_obj<typename D::access_type> &>(dest) = std::static_pointer_cast<typename D::access_type>(_m);
+			} else {
+				static_cast<dynamic_shared_obj<typename D::access_type> &>(dest) = typeid(nullptr);
+			}
 			return dest;
 		}
 
 		template<typename D>
 		D cast_with_type_s() const {
-			static_assert(std::is_base_of<dynamic_shared_obj<typename D::structure>, D>::value, "dest is not a 'dynamic_shared_obj'");
+			static_assert(std::is_base_of<dynamic_shared_obj<typename D::access_type>, D>::value, "dest is not a 'dynamic_shared_obj'");
 
 			D dest;
-			static_cast<dynamic_shared_obj<typename D::structure> &>(dest) = typeid(structure);
-			static_cast<shared_obj<typename D::structure> &>(dest) = std::dynamic_pointer_cast<typename D::structure>(_m);
+			if (_m.get()) {
+				static_cast<dynamic_shared_obj<typename D::access_type> &>(dest) = typeid(access_type);
+				static_cast<shared_obj<typename D::access_type> &>(dest) = std::dynamic_pointer_cast<typename D::access_type>(_m);
+			} else {
+				static_cast<dynamic_shared_obj<typename D::access_type> &>(dest) = typeid(nullptr);
+			}
 			return dest;
 		}
 
-		void operator=(std::shared_ptr<S> &&m) {
+		void operator=(std::shared_ptr<T> &&m) {
 			_m = std::move(m);
 		}
 
 		template<typename D>
 		D cast() const {
-			static_assert(std::is_base_of<shared_obj<typename D::structure>, D>::value, "dest is not a 'shared_obj'");
-			static_assert(!std::is_base_of<dynamic_shared_obj<typename D::structure>, D>::value, "dest cannot be a 'dynamic_shared_obj'");
+			static_assert(std::is_base_of<shared_obj<typename D::access_type>, D>::value, "dest is not a 'shared_obj'");
+			static_assert(!std::is_base_of<dynamic_shared_obj<typename D::access_type>, D>::value, "dest cannot be a 'dynamic_shared_obj'");
 
 			D dest;
-			static_cast<shared_obj<typename D::structure> &>(dest) = std::static_pointer_cast<typename D::structure>(_m);
+			if (_m.get()) {
+				static_cast<shared_obj<typename D::access_type> &>(dest) = std::static_pointer_cast<typename D::access_type>(_m);
+			}
 			return dest;
 		}
 
 		template<typename D>
 		D cast_s() const {
-			static_assert(std::is_base_of<shared_obj<typename D::structure>, D>::value, "dest is not a 'shared_obj'");
-			static_assert(!std::is_base_of<dynamic_shared_obj<typename D::structure>, D>::value, "dest cannot be a 'dynamic_shared_obj'");
+			static_assert(std::is_base_of<shared_obj<typename D::access_type>, D>::value, "dest is not a 'shared_obj'");
+			static_assert(!std::is_base_of<dynamic_shared_obj<typename D::access_type>, D>::value, "dest cannot be a 'dynamic_shared_obj'");
 
 			D dest;
-			static_cast<shared_obj<typename D::structure> &>(dest) = std::dynamic_pointer_cast<typename D::structure>(_m);
+			if (_m.get()) {
+				static_cast<shared_obj<typename D::access_type> &>(dest) = std::dynamic_pointer_cast<typename D::access_type>(_m);
+			}
 			return dest;
 		}
 
 	protected:
-		std::shared_ptr<S> _m;
+		std::shared_ptr<T> _m;
 };
 
-template <typename S>
-class dynamic_shared_obj : public shared_obj<S> {
+template <typename T>
+class dynamic_shared_obj : public shared_obj<T> {
 	protected:
 		constexpr dynamic_shared_obj() : _t(typeid(nullptr)) {}
 
 	public:
 		template<typename... A>
 		void alloc(A&&... a) {
-			_t = typeid(typename shared_obj<S>::structure);
-			shared_obj<S>::alloc(std::forward<A>(a)...);
+			_t = typeid(T);
+			shared_obj<T>::alloc(std::forward<A>(a)...);
 		}
 
 		void free() {
 			_t = typeid(nullptr);
-			shared_obj<S>::free();
+			shared_obj<T>::free();
 		}
 
 		template<typename D>
 		bool type_is() {
-			static_assert(std::is_base_of<shared_obj<typename D::structure>, D>::value, "target is not a 'shared_obj'");
+			static_assert(std::is_base_of<shared_obj<typename D::access_type>, D>::value, "target is not a 'shared_obj'");
 
-			return _t == typeid(typename D::structure);
+			return _t == typeid(typename D::access_type);
 		}
 
 		void operator=(std::type_index t) {
@@ -146,21 +158,25 @@ class dynamic_shared_obj : public shared_obj<S> {
 
 		template<typename D>
 		D cast_with_type() const {
-			static_assert(std::is_base_of<dynamic_shared_obj<typename D::structure>, D>::value, "dest is not a 'dynamic_shared_obj'");
+			static_assert(std::is_base_of<dynamic_shared_obj<typename D::access_type>, D>::value, "dest is not a 'dynamic_shared_obj'");
 
 			D dest;
-			static_cast<dynamic_shared_obj<typename D::structure> &>(dest) = _t;
-			static_cast<shared_obj<typename D::structure> &>(dest) = std::static_pointer_cast<typename D::structure>(shared_obj<S>::_m);
+			static_cast<dynamic_shared_obj<typename D::access_type> &>(dest) = _t;
+			if (shared_obj<T>::_m.get()) {
+				static_cast<shared_obj<typename D::access_type> &>(dest) = std::static_pointer_cast<typename D::access_type>(shared_obj<T>::_m);
+			}
 			return dest;
 		}
 
 		template<typename D>
 		D cast_with_type_s() const {
-			static_assert(std::is_base_of<dynamic_shared_obj<typename D::structure>, D>::value, "dest is not a 'dynamic_shared_obj'");
+			static_assert(std::is_base_of<dynamic_shared_obj<typename D::access_type>, D>::value, "dest is not a 'dynamic_shared_obj'");
 
 			D dest;
-			static_cast<dynamic_shared_obj<typename D::structure> &>(dest) = _t;
-			static_cast<shared_obj<typename D::structure> &>(dest) = std::dynamic_pointer_cast<typename D::structure>(shared_obj<S>::_m);
+			static_cast<dynamic_shared_obj<typename D::access_type> &>(dest) = _t;
+			if (shared_obj<T>::_m.get()) {
+				static_cast<shared_obj<typename D::access_type> &>(dest) = std::dynamic_pointer_cast<typename D::access_type>(shared_obj<T>::_m);
+			}
 			return dest;
 		}
 
