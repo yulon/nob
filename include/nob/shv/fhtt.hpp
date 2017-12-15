dab170d7ae75e80e956b38af9f6e7324dfb5ecf7
Yuchen Wang <yulon@me.com>
2017-12-16 05:33:45 +0800

    Updated fhtt

--- include/nob/shv/fhtt.hpp
+++ include/nob/shv/fhtt.hpp
@@ -5,7 +5,6 @@
 
 namespace nob {
 	namespace shv {
-		extern const std::array<uint64_t, 5851> _fhtt_key_list;
 		extern const std::unordered_map<uint16_t, const std::unordered_map<uint64_t, uint64_t>> fhtt;
 	} /* shv */
 } /* nob */
--- src/shv/fhtt.cpp
+++ src/shv/fhtt.cpp
@@ -2,10 +2,6 @@
 
 namespace nob {
 	namespace shv {
-		const std::array<uint64_t, 5851> _fhtt_key_list {
-			#include "fhtt/key_list.inc"
-		};
-
 		const std::unordered_map<uint16_t, const std::unordered_map<uint64_t, uint64_t>> fhtt {
 			{
 				1290,
