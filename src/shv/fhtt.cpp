4dfdeb1dfe94bb33f7857e759cd34a3d1d36364e
Yuchen Wang <yulon@me.com>
2017-12-15 17:26:06 +0800

    Use include

--- src/shv/fhtt.cpp
+++ src/shv/fhtt.cpp
@@ -2,9 +2,23 @@
 
 namespace nob {
 	namespace shv {
-		const std::unordered_map<uint16_t, const std::unordered_map<uint64_t, uint64_t> &> fhtt {
-			{ 1180, _fhtt_1180 },
-			{ 1103, _fhtt_1103 },
+		const std::array<uint64_t, 5851> _fhtt_key_list {
+			#include "fhtt/key_list.inc"
+		};
+
+		const std::unordered_map<uint16_t, const std::unordered_map<uint64_t, uint64_t>> fhtt {
+			{
+				1180,
+				{
+					#include "fhtt/1180.inc"
+				}
+			},
+			{
+				1103,
+				{
+					#include "fhtt/1103.inc"
+				}
+			}
 		};
 	} /* shv */
 } /* nob */
