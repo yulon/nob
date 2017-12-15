1ec1bd2bdcf3754cf2159d338eba7b232defc3f7
Yuchen Wang <yulon@me.com>
2017-12-15 21:59:28 +0800

    Added func_table_t::node_1290_t

--- src/ntv/var/types.cpp
+++ src/ntv/var/types.cpp
@@ -21,9 +21,16 @@ namespace nob {
 			return nullptr;
 		}
 
-		func_table_t::func_table_t() : _nodes(program::code.match_rel_ptr({
-			// Reference from https://github.com/ivanmeler/OpenVHook
-			0x76, 0x61, 0x49, 0x8B, 0x7A, 0x40, 0x48, 0x8D, 0x0D, 1111, 1111, 1111, 1111
-		})) {}
+		func_table_t::func_table_t() : _nodes(
+			program::version < 1290 ?
+			program::code.match_rel_ptr({
+				// Reference from https://github.com/ivanmeler/OpenVHook
+				0x76, 0x61, 0x49, 0x8B, 0x7A, 0x40, 0x48, 0x8D, 0x0D, 1111, 1111, 1111, 1111
+			}) :
+			program::code.match_rel_ptr({
+				// Reference from https://www.unknowncheats.me/forum/1932632-post1648.html
+				0x40, 0x53, 0x48, 0x83, 0xEC, 0x20, 0x48, 0x8D, 0x1D, 1111, 1111, 1111, 1111, 0x4C, 0x8D, 0x05
+			})
+		) {}
 	} /* ntv */
 } /* nob */
