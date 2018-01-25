eb1d4410f0d910a4c534c6343dbf373b4802bfec
Yulon <yulon@me.com>
2018-01-25 15:18:55 +0800

    Updated

--- include/nob/object.hpp
+++ include/nob/object.hpp
@@ -163,6 +163,10 @@ namespace nob {
 				ntv::ENTITY::SET_ENTITY_HEALTH(_h, v);
 			}
 
+			bool is_touching(entity target) const {
+				return ntv::ENTITY::IS_ENTITY_TOUCHING_ENTITY(_h, target);
+			}
+
 		protected:
 			int _h;
 	};
@@ -205,9 +209,11 @@ namespace nob {
 			void free();
 
 			void resurrect() {
-				ntv::ENTITY::SET_ENTITY_HEALTH(_h, ntv::ENTITY::GET_ENTITY_MAX_HEALTH(_h));
 				ntv::AI::CLEAR_PED_TASKS_IMMEDIATELY(_h);
 				ntv::ENTITY::SET_ENTITY_COLLISION(_h, true, true);
+				if (_h == ntv::PLAYER::PLAYER_PED_ID()) {
+					ntv::GAMEPLAY::_RESET_LOCALPLAYER_STATE();
+				}
 			}
 
 			void set_random_outfit() {
@@ -594,6 +600,173 @@ namespace nob {
 
 			void paralysis();
 
+			void cant_be_explode_head(bool toggle = true) {
+				ntv::PED::SET_PED_SUFFERS_CRITICAL_HITS(_h, !toggle);
+			}
+
+			bool is_in_melee_combat() const {
+				return ntv::PED::IS_PED_IN_MELEE_COMBAT(_h);
+			}
+
+			entity melee_target() const {
+				return ntv::PED::GET_MELEE_TARGET_FOR_PED(_h);
+			}
+
+			void melee(entity e) {
+				return ntv::AI::TASK_PUT_PED_DIRECTLY_INTO_MELEE(_h, e, 0.0, 0.0, 0.0, 0);
+			}
+
+			void accuracy(int val) {
+				ntv::PED::SET_PED_ACCURACY(_h, val);
+			}
+
+			int accuracy() const {
+				return ntv::PED::GET_PED_ACCURACY(_h);
+			}
+
+			static void commad_damage_modifier(float val) {
+				ntv::PED::SET_AI_WEAPON_DAMAGE_MODIFIER(val);
+				ntv::PED::SET_AI_MELEE_WEAPON_DAMAGE_MODIFIER(val);
+			}
+
+			static void reset_commad_damage_modifier() {
+				ntv::PED::RESET_AI_WEAPON_DAMAGE_MODIFIER();
+				ntv::PED::RESET_AI_MELEE_WEAPON_DAMAGE_MODIFIER();
+			}
+
+			enum class ntv_task_t : unsigned int {
+				TASK_ACHIEVE_HEADING = 0x7276d3df,
+				TASK_AIM_GUN_AT_COORD = 0x49bef36e,
+				TASK_AIM_GUN_AT_ENTITY = 0x6134071b,
+				TASK_AIM_GUN_SCRIPTED = 0x0c69931f,
+				TASK_AIM_GUN_SCRIPTED_WITH_TARGET = 0x0c69931f,
+				TASK_ARREST_PED = 0x52ff82c0,
+				TASK_BOAT_MISSION = 0xb41f1a34,
+				TASK_CHAT_TO_PED = 0x0fc239cd,
+				TASK_CLEAR_DEFENSIVE_AREA = 0x00a101c8,
+				TASK_CLIMB = 0xb802fdca,
+				TASK_CLIMB_LADDER = 0x66403353,
+				TASK_COMBAT_PED = 0x2e85a751,
+				TASK_COMBAT_HATED_TARGETS_AROUND_PED = 0xaa05b492,
+				TASK_COMBAT_HATED_TARGETS_AROUND_PED_TIMED = 0x2719c0d1,
+				TASK_COMBAT_HATED_PED_IN_AREA = 0x42cc4f21,
+				TASK_COMBAT_TIMED = 0xf2e41a8a,
+				TASK_COWER = 0x1c43f4cf,
+				TASK_DRIVE_BY = 0x7d711e7d,
+				TASK_EVERYONE_LEAVE_VEHICLE = 0xa569f146,
+				TASK_EXIT_COVER = 0x4e961d82,
+				TASK_FOLLOW_NAV_MESH_TO_COORD = 0x2a89b8a7,
+				TASK_FOLLOW_NAV_MESH_TO_COORD_ADVANCED = 0x9c4fbcac,
+				TASK_FOLLOW_POINT_ROUTE = 0xb232526f,
+				TASK_FOLLOW_TO_OFFSET_OF_ENTITY = 0x3ef867f4,
+				TASK_FOLLOW_WAYPOINT_RECORDING = 0x9b4fc7d8,
+				TASK_FORCE_MOTION_STATE = 0x9e78ac1f,
+				TASK_GET_OFF_BOAT = 0x9a27a999,
+				TASK_GO_STRAIGHT_TO_COORD = 0x7d8f4411,
+				TASK_GO_STRAIGHT_TO_COORD_RELATIVE_TO_ENTITY = 0x78ec0ff6,
+				TASK_GO_TO_COORD_AND_AIM_AT_HATED_ENTITIES_NEAR_COORD = 0x290a02bc,
+				TASK_GO_TO_COORD_ANY_MEANS = 0x93399e79,
+				TASK_GO_TO_COORD_ANY_MEANS_EXTRA_PARAMS = 0x45b5a146,
+				TASK_GO_TO_COORD_ANY_MEANS_EXTRA_PARAMS_WITH_CRUISE_SPEED = 0x4de5c290,
+				TASK_GOTO_ENTITY = 0x4924437d,
+				TASK_GOTO_ENTITY_AIMING = 0x967ea21c,
+				TASK_GOTO_ENTITY_OFFSET = 0x87e3e0a8,
+				TASK_GOTO_ENTITY_OFFSET_XY = 0x87e3e0a8,
+				TASK_GUARD_ASSIGNED_DEFENSIVE_AREA = 0xdf5f4ba7,
+				TASK_GUARD_CURRENT_POS = 0x8ce49d34,
+				TASK_GUARD_SPHERE_DEFENSIVE_AREA = 0x21e8d4e4,
+				TASK_HANDS_UP = 0xa573b67c,
+				TASK_HELI_CHASE = 0x27369192,
+				TASK_HELI_MISSION = 0xb41f1a34,
+				TASK_VEHICLE_HELI_PROTECT = 0xb41f1a34,
+				TASK_JUMP = 0x24415046,
+				TASK_LEAVE_ANY_VEHICLE = 0xce98fbb3,
+				TASK_LEAVE_VEHICLE = 0x1ae73569,
+				TASK_MOVE_NETWORK_ADVANCED = 0x0494661c,
+				TASK_MOVE_NETWORK = 0x0494661c,
+				TASK_PARACHUTE = 0x76ca4a8e,
+				TASK_PARACHUTE_TO_TARGET = 0x4921b47a,
+				TASK_PATROL = 0xb550726c,
+				TASK_PAUSE = 0x03c990ec,
+				TASK_PED_SLIDE_TO_COORD_HDG_RATE = 0x3e5094a7,
+				TASK_PERFORM_SEQUENCE = 0x0e763797,
+				TASK_PERFORM_SEQUENCE_FROM_PROGRESS = 0x5485fd94,
+				TASK_PLANE_CHASE = 0x02dba9bf,
+				TASK_PLANE_CHASE_2 = 0x043e4a56,
+				TASK_PLANE_MISSION = 0xb41f1a34,
+				TASK_PLANT_BOMB = 0x8127fd1a,
+				TASK_PUT_PED_DIRECTLY_INTO_COVER = 0x8b2f140e,
+				TASK_PUT_PED_DIRECTLY_INTO_MELEE = 0xfbbf6f4d,
+				TASK_RAPPEL_FROM_HELI = 0xef8d6b40,
+				TASK_REACT_AND_FLEE_PED = 0x7dedf098,
+				TASK_RELOAD_WEAPON = 0xc322ed6f,
+				TASK_SCRIPTED_ANIMATION = 0x87b9a382,
+				TASK_SEEK_COVER_FROM_PED = 0x71e30bdc,
+				TASK_SEEK_COVER_FROM_POS = 0xa77a06c5,
+				TASK_SEEK_COVER_TO_COVER_COORD = 0x6c01775c,
+				TASK_SEEK_COVER_TO_COVER_POINT = 0x99afa8a3,
+				TASK_SET_BLOCKING_OF_NON_TEMPORARY_EVENTS = 0x6f9c865c,
+				TASK_SET_DECISION_MAKER = 0x4e5b453c,
+				TASK_SET_PED_SPHERE_DEFENSIVE_AREA = 0x00a101c8,
+				TASK_SHOCKING_EVENT_REACT = 0x498babe3,
+				TASK_SHOOT_GUN_AT_COORD = 0xd90ef188,
+				TASK_SHOOT_AT_ENTITY = 0x0a01f8b8,
+				TASK_SHUFFLE_TO_NEXT_VEHICLE_SEAT = 0x153011fc,
+				TASK_SKY_DIVE = 0x4b65f15c,
+				TASK_SMART_FLEE_POINT = 0xf10822aa,
+				TASK_SMART_FLEE_PED = 0x6ba30179,
+				TASK_STAND_GUARD = 0xd88f2cde,
+				TASK_STAND_STILL = 0xc572e06a,
+				TASK_START_SCENARIO_AT_POSITION = 0xbe86c566,
+				TASK_START_SCENARIO_IN_PLACE = 0x3b3a458f,
+				TASK_STAY_IN_COVER = 0xe1c16e99,
+				TASK_STEALTH_KILL = 0x5014cc1a,
+				TASK_SWAP_WEAPON = 0x2ab81462,
+				TASK_GENERAL_SWEEP = 0x491a782d,
+				TASK_GENERAL_SWEEP_2 = 0x491a782d,
+				TASK_SYNCHRONIZED_SCENE = 0x6a67a5cc,
+				TASK_THROW_PROJECTILE = 0xad37bf03,
+				TASK_TOGGLE_DUCK = 0x0f3b8554,
+				TASK_TURN_PED_TO_FACE_COORD = 0x574bb8f5,
+				TASK_TURN_PED_TO_FACE_ENTITY = 0xcbce4595,
+				TASK_USE_MOBILE_PHONE = 0x37d339a1,
+				TASK_USE_MOBILE_PHONE_TIMED = 0x37d339a1,
+				TASK_USE_NEAREST_SCENARIO_CHAIN_TO_COORD = 0x9bd19ae7,
+				TASK_USE_NEAREST_SCENARIO_CHAIN_TO_COORD_WARP = 0x9bd19ae7,
+				TASK_USE_NEAREST_SCENARIO_TO_COORD = 0x623a5efe,
+				TASK_USE_NEAREST_SCENARIO_TO_COORD_WARP = 0x623a5efe,
+				TASK_VEHICLE_CHASE = 0x2288a57c,
+				TASK_VEHICLE_DRIVE_TO_COORD_LONGRANGE = 0x21d33957,
+				TASK_VEHICLE_DRIVE_TO_COORD = 0x93a5526e,
+				TASK_VEHICLE_DRIVE_WANDER = 0xf09b15b3,
+				TASK_VEHICLE_ESCORT = 0xb41f1a34,
+				TASK_VEHICLE_FOLLOW = 0xb41f1a34,
+				TASK_VEHICLE_FOLLOW_WAYPOINT_RECORDING = 0xf1f17ae7,
+				TASK_VEHICLE_GOTO_NAVMESH = 0xfbb43c4a,
+				TASK_VEHICLE_MISSION_PED_TARGET = 0xb41f1a34,
+				TASK_VEHICLE_MISSION = 0xb41f1a34,
+				TASK_VEHICLE_PARK = 0xefc8537e,
+				TASK_VEHICLE_TEMP_ACTION = 0x81b4d53a,
+				TASK_WANDER_IN_AREA = 0x370bcf53,
+				TASK_WANDER_STANDARD = 0xbba3b7ca,
+				TASK_WARP_PED_INTO_VEHICLE = 0xbc555b9d,
+				TASK_WRITHE = 0x8ec23e41,
+				TASK_VEHICLE_AIM_AT_COORD = 0x00c59c52,
+				TASK_VEHICLE_AIM_AT_PED = 0x6f30f4c1,
+				TASK_VEHICLE_SHOOT_AT_COORD = 0xaf18b824,
+				TASK_VEHICLE_SHOOT_AT_PED = 0x20123810,
+				_TASK_ENTER_VEHICLE = 0x950b6492,
+				_TASK_PERFORM_SEQUENCE = 0xe763797
+			};
+
+			int ntv_task_state(ntv_task_t t) const {
+				return nob::ntv::AI::GET_SCRIPT_TASK_STATUS(_h, static_cast<uint32_t>(t));
+			}
+
+			bool has_ntv_task(ntv_task_t t) const {
+				return ntv_task_state(t) != 7;
+			}
+
 			////////////////////////////////////////////////////////////////////
 
 			class group {
@@ -671,7 +844,7 @@ namespace nob {
 		}
 
 		inline void change_body(character chr) {
-			ntv::PLAYER::CHANGE_PLAYER_PED(native_handle(), chr, true, true);
+			ntv::PLAYER::CHANGE_PLAYER_PED(0, chr, true, true);
 		}
 
 		inline void switch_body(character chr) {
@@ -701,8 +874,16 @@ namespace nob {
 			ntv::GAMEPLAY::_DISABLE_AUTOMATIC_RESPAWN(true);
 		}
 
-		inline void clear_state() {
-			ntv::GAMEPLAY::_RESET_LOCALPLAYER_STATE();
+		inline entity aiming_entity() {
+			int e;
+			ntv::PLAYER::GET_ENTITY_PLAYER_IS_FREE_AIMING_AT(0, &e);
+			return entity(e);
+		}
+
+		inline void damage_modifier(float val) {
+			ntv::PLAYER::SET_PLAYER_WEAPON_DAMAGE_MODIFIER(0, val);
+			ntv::PLAYER::SET_PLAYER_MELEE_WEAPON_DAMAGE_MODIFIER(0, val);
+			ntv::PLAYER::SET_PLAYER_VEHICLE_DAMAGE_MODIFIER(0, val);
 		}
 	}
 
@@ -790,6 +971,10 @@ namespace nob {
 					ntv::AUDIO::SET_VEHICLE_RADIO_LOUD(_h, true);
 				}
 				ntv::AUDIO::SET_VEH_RADIO_STATION(_h, rs.c_str());
+			}
+
+			void engine_on() {
+				ntv::VEHICLE::SET_VEHICLE_ENGINE_ON(_h, true, true, true);
 			}
 
 			static constexpr float min_engine_health = -4000.0f;
@@ -896,8 +1081,12 @@ namespace nob {
 				return static_cast<klass_t>(ntv::VEHICLE::GET_VEHICLE_CLASS(_h));
 			}
 
-			bool is_armed() {
+			bool is_armed() const {
 				return ntv::VEHICLE::DOES_VEHICLE_HAVE_WEAPONS(_h);
+			}
+
+			void special_ability_bar_active(bool toggle = true) {
+				ntv::VEHICLE::_SET_VEHICLE_HUD_SPECIAL_ABILITY_BAR_ACTIVE(_h, toggle);
 			}
 	};
 
@@ -942,6 +1131,82 @@ namespace nob {
 
 			bool has_landing_gear() {
 				return ntv::VEHICLE::_VEHICLE_HAS_LANDING_GEAR(_h);
+			}
+	};
+
+	class helicopter : public plane {
+		public:
+			using plane::plane;
+
+			/*
+				Max 1000.
+				At -100 both helicopter rotors will stall.
+			*/
+
+			float engine_health() {
+				return ntv::VEHICLE::_GET_HELI_ENGINE_HEALTH(_h);
+			}
+
+			/*
+				Max 1000.
+				At 0 the tail rotor will stall.
+			*/
+
+			float rotor_health() {
+				return ntv::VEHICLE::_GET_HELI_MAIN_ROTOR_HEALTH(_h);
+			}
+
+			float tail_rotor_health() {
+				return ntv::VEHICLE::_GET_HELI_TAIL_ROTOR_HEALTH(_h);
+			}
+
+			void searchlight(bool toggle = true) {
+				ntv::VEHICLE::SET_VEHICLE_SEARCHLIGHT(_h, toggle, false);
+			}
+
+			void engine_on(float val = 1.0f) {
+				ntv::VEHICLE::_SET_HELICOPTER_ROLL_PITCH_YAW_MULT(_h, val);
+			}
+	};
+
+	class cargobob : public helicopter {
+		public:
+			using helicopter::helicopter;
+
+			void detach(vehicle veh) {
+				if (_h) {
+					ntv::VEHICLE::DETACH_VEHICLE_FROM_CARGOBOB(veh, _h);
+				} else {
+					ntv::VEHICLE::DETACH_VEHICLE_FROM_ANY_CARGOBOB(veh);
+				}
+			}
+
+			bool is_attached(vehicle veh) const {
+				return ntv::VEHICLE::IS_VEHICLE_ATTACHED_TO_CARGOBOB(_h, veh);
+			}
+
+			vehicle attached() const {
+				return ntv::VEHICLE::GET_VEHICLE_ATTACHED_TO_CARGOBOB(_h);
+			}
+
+			void attach(vehicle veh) {
+				ntv::VEHICLE::ATTACH_VEHICLE_TO_CARGOBOB(veh, _h, 0, 0, 0, 0);
+			}
+
+			void put_down_hook() {
+				ntv::VEHICLE::CREATE_PICK_UP_ROPE_FOR_CARGOBOB(_h, 0);
+			}
+
+			void put_down_magnet() {
+				ntv::VEHICLE::CREATE_PICK_UP_ROPE_FOR_CARGOBOB(_h, 1);
+			}
+
+			void magnet_strength(float val) {
+				ntv::VEHICLE::_SET_CARGOBOB_PICKUP_MAGNET_STRENGTH(_h, val);
+			}
+
+			bool has_magnet() const {
+				return ntv::VEHICLE::_DOES_CARGOBOB_HAVE_PICKUP_MAGNET(_h);
 			}
 	};
 
