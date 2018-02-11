4a6f1b83f7955c990e0e972b971e71eb23d26ea5
Yulon <yulon@me.com>
2018-02-11 13:53:38 +0800

    Improved vector3

--- include/nob/vector.hpp
+++ include/nob/vector.hpp
@@ -12,6 +12,22 @@ namespace nob {
 
 	struct vector2 {
 		float x, y;
+
+		vector2() = default;
+
+		constexpr vector2(float x_val, float y_val) : x(x_val), y(y_val) {}
+
+		constexpr vector2(std::initializer_list<float> vals) :
+			x(vals.size() ? *vals.begin() : 0.0f),
+			y(vals.size() >= 2 ? vals.begin()[1] : 0.0f)
+		{}
+
+		float distance(const vector2 &v2) const {
+			return std::sqrt(
+				std::pow((double)x - (double)v2.x, 2) +
+				std::pow((double)y - (double)v2.y, 2)
+			);
+		}
 	};
 
 	#ifndef _NOB_NTV_VEC3
@@ -23,14 +39,19 @@ namespace nob {
 		}
 	#endif
 
-	struct vector3 {
-		float x, y, z;
+	struct vector3 : public vector2 {
+		float z;
 
 		vector3() = default;
 
-		constexpr vector3(float x_val, float y_val, float z_val = 0) : x(x_val), y(y_val), z(z_val) {}
+		constexpr vector3(float x_val, float y_val, float z_val = 0.0f) : vector2(x_val, y_val), z(z_val) {}
 
-		constexpr vector3(std::initializer_list<float> vals) : vector3(*vals.begin(), vals.begin()[1], vals.begin()[2]) {}
+		constexpr vector3(const vector2 &v2, float z_val = 0.0f) : vector2(v2), z(z_val) {}
+
+		constexpr vector3(std::initializer_list<float> vals) :
+			vector2(vals),
+			z(vals.size() >= 3 ? vals.begin()[2] : 0.0f)
+		{}
 
 		constexpr vector3(const ntv::Vector3 &nv3) : vector3(nv3.x, nv3.y, nv3.z) {}
 
