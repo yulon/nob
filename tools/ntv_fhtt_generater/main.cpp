2b78715056276900f5e91163e46fa408a68c05c1
Yulon <yulon@me.com>
2018-03-26 14:25:08 +0800

    Improved nob_ntv_fhtt_generater

--- tools/ntv_fhtt_generater/main.cpp
+++ tools/ntv_fhtt_generater/main.cpp
@@ -10,14 +10,14 @@
 
 std::unordered_set<uint64_t> old_hashs, new_hashs;
 
-std::unordered_map<std::string, std::vector<uintptr_t>> old_ysc_hashs_map, new_ysc_hashs_map;
+std::unordered_map<std::string, std::pair<size_t, std::vector<uintptr_t>>> old_ysc_map, new_ysc_map;
 
 const std::unordered_map<uint64_t, uint64_t> *old_fhtt;
 std::unordered_map<uint64_t, std::map<size_t, uint64_t>> new_fhtt;
 
 namespace fs = std::experimental::filesystem;
 
-void load_yscs(std::string dir, std::unordered_set<uint64_t> &hashs, std::unordered_map<std::string, std::vector<uintptr_t>> &ysc_hashs_map) {
+void load_yscs(std::string dir, std::unordered_set<uint64_t> &hashs, std::unordered_map<std::string, std::pair<size_t, std::vector<uintptr_t>>> &ysc_map) {
 	for (auto &de: std::experimental::filesystem::directory_iterator(dir)) {
 		auto path = de.path().string();
 		auto name = de.path().filename().string();
@@ -35,11 +35,13 @@ void load_yscs(std::string dir, std::unordered_set<uint64_t> &hashs, std::unorde
 
 		ysc.redirect();
 
-		auto &ysc_hashs = ysc_hashs_map[name];
+		auto &ysc_inf = ysc_map[name];
+
+		ysc_inf.first = ysc.code_size;
 
 		for (size_t i = 0; i < ysc.using_func_count; ++i) {
 			hashs.emplace(ysc.using_funcs[i]);
-			ysc_hashs.emplace_back(ysc.using_funcs[i]);
+			ysc_inf.second.emplace_back(ysc.using_funcs[i]);
 		}
 	}
 }
@@ -56,17 +58,17 @@ int main() {
 
 	auto bin_dir_path = fs::path(rua::process::from_this().file_path()).remove_filename().string();
 
-	std::cout << "reading '" + bin_dir_path + "ysc\\{" << old_ver << "," << new_ver << "}\\*' ysc files" << std::endl;
+	std::cout << "reading ysc files from '" + bin_dir_path + "ysc\\{" << old_ver << "," << new_ver << "}\\*'" << std::endl;
 
-	load_yscs(bin_dir_path + "ysc\\" + std::to_string(old_ver), old_hashs, old_ysc_hashs_map);
-	load_yscs(bin_dir_path + "ysc\\" + std::to_string(new_ver), new_hashs, new_ysc_hashs_map);
+	load_yscs(bin_dir_path + "ysc\\" + std::to_string(old_ver), old_hashs, old_ysc_map);
+	load_yscs(bin_dir_path + "ysc\\" + std::to_string(new_ver), new_hashs, new_ysc_map);
 
 	std::cout << "old_hashs: " << old_hashs.size() << std::endl;
 	std::cout << "new_hashs: " << new_hashs.size() << std::endl;
 
-	std::cout << "coverting hashs" << std::endl;
+	std::cout << "converting hashs" << std::endl;
 
-	std::cout << std::hex << std::uppercase;
+	size_t loss_old_hash_c = 0;
 
 	for (auto &new_hash : new_hashs) {
 		struct point_t {
@@ -76,9 +78,9 @@ int main() {
 
 		std::vector<point_t> points;
 
-		for (auto &pr : new_ysc_hashs_map) {
-			for (size_t i = 0; i < pr.second.size(); ++i) {
-				if (pr.second[i] == new_hash) {
+		for (auto &pr : new_ysc_map) {
+			for (size_t i = 0; i < pr.second.second.size(); ++i) {
+				if (pr.second.second[i] == new_hash) {
 					points.emplace_back(point_t{ pr.first, i });
 				}
 			}
@@ -87,72 +89,66 @@ int main() {
 		std::unordered_map<uintptr_t, size_t> old_hash_ballot;
 
 		for (auto &pt : points) {
-			auto &old_ysc_hashs = old_ysc_hashs_map[pt.ysc_name];
-			auto &new_ysc_hashs = new_ysc_hashs_map[pt.ysc_name];
+			auto &old_ysc = old_ysc_map[pt.ysc_name];
+			auto &new_ysc = new_ysc_map[pt.ysc_name];
 
 			int ix = pt.ix;
 
-			if (pt.ix >= old_ysc_hashs.size()) {
-				ix = old_ysc_hashs.size() - 1;
+			if (pt.ix >= old_ysc.second.size()) {
+				ix = old_ysc.second.size() - 1;
 			}
 
-			auto &vc = ++old_hash_ballot[old_ysc_hashs[ix]];
+			auto &vc = ++old_hash_ballot[old_ysc.second[ix]];
 
-			if (new_ysc_hashs.size() > old_ysc_hashs.size()) {
-				int n = new_ysc_hashs.size() - old_ysc_hashs.size();
-				if (pt.ix >= old_ysc_hashs.size()) {
-					n -= pt.ix - (old_ysc_hashs.size() - 1);
+			if (new_ysc.second.size() > old_ysc.second.size()) {
+				int n = new_ysc.second.size() - old_ysc.second.size();
+				if (pt.ix >= old_ysc.second.size()) {
+					n -= pt.ix - (old_ysc.second.size() - 1);
 				}
 				for (int i = ix - 1; i > ix - n - 1 && i >= 0; --i) {
-					++old_hash_ballot[old_ysc_hashs[i]];
+					++old_hash_ballot[old_ysc.second[i]];
 				}
-			} else if (new_ysc_hashs.size() < old_ysc_hashs.size()) {
-				int n = old_ysc_hashs.size() - new_ysc_hashs.size();
-				for (int i = ix + 1; i < ix + n && i < (int)old_ysc_hashs.size(); ++i) {
-					++old_hash_ballot[old_ysc_hashs[i]];
+			} else if (new_ysc.second.size() < old_ysc.second.size()) {
+				int n = old_ysc.second.size() - new_ysc.second.size();
+				for (int i = ix + 1; i < ix + n && i < (int)old_ysc.second.size(); ++i) {
+					++old_hash_ballot[old_ysc.second[i]];
 				}
 			} else {
-				if (vc == 1) {
-					++vc;
+				if (new_ysc.first == old_ysc.first && vc == 1) {
+					vc += 100000;
 				}
 			}
 		}
 
-		size_t old_hash_votes = 0;
-		uintptr_t old_hash = 0;
-
-		for (auto &votes : old_hash_ballot) {
-			if (votes.second > old_hash_votes) {
-				old_hash_votes = votes.second;
-				old_hash = votes.first;
-			}
-		}
-
-		if (!old_hash) {
-			std::cout << "!old_hash " << new_hash << std::endl;
+		if (old_hash_ballot.empty()) {
+			++loss_old_hash_c;
 			new_fhtt[new_hash].emplace(1, new_hash);
 			continue;
 		}
 
 		for (auto &hash_pr : *old_fhtt) {
-			if (hash_pr.second == old_hash) {
-				auto &map = new_fhtt[hash_pr.first];
-				auto it = map.find(old_hash_votes);
-				if (it == map.end()) {
-					map.emplace(old_hash_votes, new_hash);
+			auto &map = new_fhtt[hash_pr.first];
+
+			for (auto &votes : old_hash_ballot) {
+				if (votes.first == hash_pr.second) {
+					auto it = map.find(votes.second);
+					if (it == map.end()) {
+						map.emplace(votes.second, new_hash);
+					}
 				}
-				break;
 			}
 		}
+
+		Sleep(1);
 	}
 
-	std::cout << std::dec;
+	std::cout << "loss_old_hash_c: " << loss_old_hash_c << std::endl;
 
 	std::cout << "new_fhtt: " << new_fhtt.size() << std::endl;
 
-	auto new_fhtt_path = bin_dir_path + "..\\src\\ntv\\fhtt\\" + std::to_string(new_ver) + ".auto.inc";
+	auto new_fhtt_path = bin_dir_path + "..\\src\\ntv\\fhtt\\" + std::to_string(new_ver) + ".unsafe.inc";
 
-	std::cout << "writing '" << new_fhtt_path << "'" << std::endl;
+	std::cout << "writing new_fhtt to '" << new_fhtt_path << "'" << std::endl;
 
 	std::fstream fhtt_f(new_fhtt_path, std::ios_base::binary | std::ios_base::trunc | std::ios_base::out);
 	if (!fhtt_f.is_open()) {
@@ -161,18 +157,31 @@ int main() {
 
 	fhtt_f << std::hex << std::uppercase;
 
+	size_t miti_new_hash_c = 0;
+
 	bool first = true;
 
 	for (auto &pr : new_fhtt) {
+		if (pr.second.empty()) {
+			continue;
+		}
+
 		if (first) {
 			first = false;
 		} else {
 			fhtt_f << "," << std::endl;
 		}
+
 		fhtt_f << "{ 0x" << std::setw(16) << std::setfill('0') << pr.first << ", 0x" << std::setw(16) << std::setfill('0') << pr.second.rbegin()->second << " }";
+
+		if (pr.second.size() > 1) {
+			++miti_new_hash_c;
+		}
 	}
 
 	fhtt_f << std::endl;
+
+	std::cout << "miti_new_hash_c: " << miti_new_hash_c << std::endl;
 
 	std::cout << "done!!!" << std::endl;
 }
