c1eb4ff983d87cc6a3bd95cf36d09e85ddc275b4
Yulon <yulon@me.com>
2018-03-29 21:48:22 +0800

    Changed build configs

--- premake5.lua
+++ premake5.lua
@@ -1,78 +1,33 @@
 workspace "nob"
-	language "C++"
-	cppdialect "C++17"
 	location ( "build" )
-	configurations { "Debug", "Release" }
-	platforms {"x64"}
 	objdir("build/obj")
-	includedirs { "include", "deps" }
 
-	project "nob"
-		kind "StaticLib"
+	dofileopt "include.pm5"
+
+	platforms { "x64" }
+	configurations { "Debug", "Release" }
+
+	configuration "Debug"
+		targetsuffix "_d"
+		defines { "DEBUG" }
+		symbols "On"
+		warnings "Extra"
+
+	configuration "Release"
+		defines { "NDEBUG" }
+		optimize "Speed"
+
+	configuration { "not StaticLib" }
+		targetdir("bin")
+
+	configuration { "StaticLib" }
 		targetdir("lib")
-		files {
-			"include/*.hpp", "include/*.h",
 
-			"include/nob/*.hpp", "include/nob/*.h",
+	configuration { "windows", "gmake*", "StaticLib" }
+		targetprefix "lib"
+		targetextension ".a"
 
-			"include/nob/ntv/*.hpp", "include/nob/ntv/*.h",
+	dofileopt "project.pm5"
 
-			"include/nob/shv/*.hpp", "include/nob/shv/*.h",
-
-			"src/*.cpp", "src/*.hpp", "src/*.h",
-
-			"src/ntv/*.cpp", "src/ntv/*.hpp", "src/ntv/*.h",
-			"src/ntv/fhtt/*.inc"
-		}
-
-		configuration { "gmake*" }
-			targetprefix "lib"
-			targetextension ".a"
-
-		configuration "Debug"
-			defines { "DEBUG" }
-			symbols "On"
-			warnings "Extra"
-
-		configuration "Release"
-			defines { "NDEBUG" }
-			optimize "Speed"
-
-	project "nob_tester"
-		kind "SharedLib"
-		targetdir("bin")
-		files { "tools/tester/*.cpp" }
-		targetextension ".asi"
-		libdirs { "lib" }
-		links { "nob", "psapi", "version", "MinHook" }
-
-		configuration { "gmake*" }
-			linkoptions { "-Wl,--exclude-all-symbols", "-static" }
-
-		configuration { "vs*" }
-			linkoptions { "/include:DllMain" }
-
-		configuration "Debug"
-			defines { "DEBUG" }
-			symbols "On"
-			warnings "Extra"
-
-		configuration "Release"
-			defines { "NDEBUG" }
-			optimize "Speed"
-
-	project "nob_ntv_fhtt_generator"
-		kind "ConsoleApp"
-		targetdir("bin")
-		files { "tools/ntv_fhtt_generator/*.cpp" }
-		libdirs { "lib" }
-		links { "nob", "psapi", "version", "stdc++fs" }
-
-		configuration "Debug"
-			defines { "DEBUG" }
-			symbols "On"
-			warnings "Extra"
-
-		configuration "Release"
-			defines { "NDEBUG" }
-			optimize "Speed"
+	dofileopt "tools/tester.pm5"
+	dofileopt "tools/ntv_fhtt_generator.pm5"
