71eb22d129403a4ac09d5c21e6765fd3b809d59a
Yulon <yulon@me.com>
2019-07-31 01:11:53 +0800

    Updated

--- src/ntv/fhtt.cpp
+++ src/ntv/fhtt.cpp
@@ -3,59 +3,45 @@
 
 #include <array>
 
-namespace nob {
-	namespace ntv {
-		const std::unordered_map<uint64_t, uint64_t> _fhtt_1103 {
-			#include "fhtt/1103.inc"
-		};
+namespace nob { namespace ntv {
 
-		const std::unordered_map<uint64_t, uint64_t> _fhtt_1180 {
-			#include "fhtt/1180.inc"
-		};
+const std::unordered_map<uint64_t, uint64_t> _fhtt_1103{
+#include "fhtt/1103.inc"
+};
 
-		const std::unordered_map<uint64_t, uint64_t> _fhtt_1290 {
-			#include "fhtt/1290.inc"
-		};
+const std::unordered_map<uint64_t, uint64_t> _fhtt_1180{
+#include "fhtt/1180.inc"
+};
 
-		const std::unordered_map<uint64_t, uint64_t> _fhtt_1365 {
-			#include "fhtt/1365.inc"
-		};
+const std::unordered_map<uint64_t, uint64_t> _fhtt_1290{
+#include "fhtt/1290.inc"
+};
 
-		const std::unordered_map<uint64_t, uint64_t> _fhtt_1493 {
-			#include "fhtt/1493.inc"
-		};
+const std::unordered_map<uint64_t, uint64_t> _fhtt_1365{
+#include "fhtt/1365.inc"
+};
 
-		const std::unordered_map<uint64_t, uint64_t> _fhtt_1604 {
-			#include "fhtt/1604.inc"
-		};
+const std::unordered_map<uint64_t, uint64_t> _fhtt_1493{
+#include "fhtt/1493.inc"
+};
 
-		const std::map<uint16_t, const std::unordered_map<uint64_t, uint64_t> &> fhtt_map {
-			{
-				1604,
-				_fhtt_1604
-			},
-			{
-				1493,
-				_fhtt_1493
-			},
-			{
-				1365,
-				_fhtt_1365
-			},
-			{
-				1290,
-				_fhtt_1290
-			},
-			{
-				1180,
-				_fhtt_1180
-			},
-			{
-				1103,
-				_fhtt_1103
-			}
-		};
+const std::unordered_map<uint64_t, uint64_t> _fhtt_1604{
+#include "fhtt/1604.inc"
+};
 
-		const std::unordered_map<uint64_t, uint64_t> *fhtt = nullptr;
-	}
-}
+const std::unordered_map<uint64_t, uint64_t> _fhtt_1737{
+#include "fhtt/1737.inc"
+};
+
+const std::map<uint16_t, const std::unordered_map<uint64_t, uint64_t> &>
+	fhtt_map{{1737, _fhtt_1737},
+			 {1604, _fhtt_1604},
+			 {1493, _fhtt_1493},
+			 {1365, _fhtt_1365},
+			 {1290, _fhtt_1290},
+			 {1180, _fhtt_1180},
+			 {1103, _fhtt_1103}};
+
+const std::unordered_map<uint64_t, uint64_t> *fhtt = nullptr;
+
+}} // namespace nob::ntv
